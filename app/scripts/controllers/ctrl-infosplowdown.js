56e0edf024977daeec27727563b58042e565e582
capic <vcapic@gmail.com>
2015-09-18 22:21:56 +0200

    changes

--- app/scripts/controllers/ctrl-infosplowdown.js
+++ app/scripts/controllers/ctrl-infosplowdown.js
@@ -13,29 +13,53 @@ angular.module('plowshareFrontApp')
       $scope.autoscroll = true;
 
       function onevent(args) {
-        $scope.download.infosPlowdown += args[0].last_infos_plowdown;
-        $scope.download.sizeFile = args[0].size_file;
-        $scope.download.sizeFileDownloaded = args[0].size_file_downloaded;
-        $scope.download.sizePart = args[0].size_part;
-        $scope.download.sizePartDownloaded = args[0].size_part_downloaded;
-        $scope.download.progressFile = args[0].progress_file;
-        $scope.download.progressPart = args[0].progress_part;
-        $scope.download.status = args[0].status;
-        $scope.download.currentSpeed = args[0].current_speed;
-        $scope.download.timeLeft = args[0].time_left;
-        $scope.download.timeSpent = args[0].time_spent;
+        var down = angular.fromJson(args[0]);
+
+        $scope.download.size_file = down.size_file;
+        $scope.download.size = down.size;
+        $scope.download.size_file_downloaded = down.size_file_downloaded;
+        $scope.download.size_part = down.size_part;
+        $scope.download.size_part_downloaded = down.size_part_downloaded;
+        $scope.download.progress_file = down.progress_file;
+        $scope.download.progress_part = down.progress_part;
+        $scope.download.status = down.status;
+        $scope.download.current_speed = down.current_speed;
+        $scope.download.time_left = down.time_left;
+        $scope.download.time_spent = down.time_spent;
+        $scope.download.theorical_start_datetime = down.theorical_start_datetime;
+
+        if ($scope.startCounter == 0 && (Date.parse($scope.download.theorical_start_datetime) > new Date().getTime())) {
+          $scope.startCounter = (Date.parse($scope.download.theorical_start_datetime) - new Date().getTime()) / 1000;
+        }
+
+        if (down.status == 3) { //TODO: utiliser une constante
+          $wamp.unsubscribe('plow.downloads.download.' + download.id);
+          $wamp.unsubscribe('plow.downloads.logs.' + download.id);
+        }
+      }
+
+      function oneventLogs(args) {
+        var downLogs = angular.fromJson(args[0]);
+        $scope.download.logs += downLogs.logs;
       }
 
       $scope.download = download;
+      $scope.download.logs = '';
       $scope.downloadPriorities = downloadPriorities;
       $scope.downloadPriority = {};
+      $scope.startCounter = 0;
       $scope.downloadPriority.selected = $filter('filter')(downloadPriorities, { id: $scope.download.priority })[0];
+
+      if (Date.parse($scope.download.theorical_start_datetime) > new Date().getTime()) {
+        $scope.startCounter = (Date.parse($scope.download.theorical_start_datetime) - new Date().getTime()) / 1000;
+      }
 
       DownloadResourceFctry.logs({Id: download.id},
         function (response) {
           if (response != '') {
-            $scope.download.infosPlowdown = response.logs;
+            $scope.download.logs = response.logs;
             $wamp.subscribe('plow.downloads.download.' + download.id, onevent);
+            $wamp.subscribe('plow.downloads.logs.' + download.id, oneventLogs);
           } else {
             $translate('infosPlowdown.form.NO_INFO').then(function (translation) {
               $scope.download.infosPlowdown = translation;
--- app/scripts/services/fcty-linkresource.js
+++ app/scripts/services/fcty-linkresource.js
@@ -10,21 +10,21 @@
 angular.module('plowshareFrontApp')
   .factory('LinkResourceFctry', ['$resource', 'settings', function ($resource, settings) {
     return $resource(
-      settings.SERVER_ADDRESS + '/api/links/:Id',
+      settings.SERVER_ADDRESS + 'links/:Id',
       {Id: '@Id'},
       {
-        'status': {url: settings.SERVER_ADDRESS + '/api/links/status', method: 'GET', isArray: true},
+        'status': {url: settings.SERVER_ADDRESS + 'links/status', method: 'GET', isArray: true},
         'search': {
-          url: settings.SERVER_ADDRESS + '/api/links/search/:Name',
+          url: settings.SERVER_ADDRESS + 'links/search/:Name',
           method: 'GET',
           params: {Name: '@Name'},
           isArray: true
         },
-        'refresh': {url: settings.SERVER_ADDRESS + '/api/links/refresh/:Id', method: 'GET', params: {Id: '@Id'}},
+        'refresh': {url: settings.SERVER_ADDRESS + 'links/refresh/:Id', method: 'GET', params: {Id: '@Id'}},
         'update': {method: 'PUT'},
-        'remove': {url: settings.SERVER_ADDRESS + '/api/links/remove', method: 'POST'},
-        'addDownloadFromLink': {url: settings.SERVER_ADDRESS + '/api/links/addDownloadFromLink', method: 'POST'},
-        'addDownloadsFromLinks': {url: settings.SERVER_ADDRESS + '/api/links/addDownloadsFromLinks', method: 'POST'}
+        'remove': {url: settings.SERVER_ADDRESS + 'links/remove', method: 'POST'},
+        'addDownloadFromLink': {url: settings.SERVER_ADDRESS + 'links/addDownloadFromLink', method: 'POST'},
+        'addDownloadsFromLinks': {url: settings.SERVER_ADDRESS + 'links/addDownloadsFromLinks', method: 'POST'}
       }
     );
   }
