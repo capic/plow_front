a7f91961722d318be324f38d7ce3176765436587
capic <vcapic@gmail.com>
2016-01-13 20:47:46 +0100

    probleme avec les package

--- app/scripts/controllers/ctrl-infosaction.js
+++ app/scripts/controllers/ctrl-infosaction.js
@@ -8,12 +8,32 @@
  * Controller of the plowshareFrontApp
  */
 angular.module('plowshareFrontApp')
-  .controller('InfosActionCtrl', ['$scope', 'ActionTypeResourceFctry',
-    function ($scope, ActionTypeResourceFctry) {
+  .controller('InfosActionCtrl', ['$scope', 'ActionTypeResourceFctry', 'DirectoryResourceFctry',
+    function ($scope, ActionTypeResourceFctry, DirectoryResourceFctry) {
       $scope.actionTypesList = [];
       $scope.actionType = {};
+      $scope.actionProperty = {};
+      $scope.listPath = [];
+
       ActionTypeResourceFctry.query({'action_type_has_property.editable': true}, function (response) {
         $scope.actionTypesList = response;
       });
+
+      $scope.directoryQueryPromise = DirectoryResourceFctry.query(
+        function (response) {
+          $scope.listPath = response;
+        }
+      );
+
+      $scope.ok = function () {
+        var action = {
+          // TODO: utiliser une constante
+          action_status_id: 1,
+          action_type_id: $scope.actionType.id,
+          action_has_properties: [$scope.actionProperty]
+
+        };
+        console.log(action);
+      }
 
     }]);
